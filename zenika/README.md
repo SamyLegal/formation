ce07928ec984bebd7975daa0b8ac181bd3d7eb51
legal-s <legal-s@mgdis.fr>
2015-11-09 00:13:58 +0100

    Clean

--- zenika/README.md
+++
@@ -1,3 +0,0 @@
-# Formation NodeJS - Zenika
-
-Repository comprend des exemples de code mis en oeuvre lors de la formation de Zenika sur NodeJs
--- zenika/nodejs/tp8/index.js
+++
@@ -1,5 +0,0 @@
-var router = require('./router');
-var Contacts = require('./lib/contacts');
-
-// Initialisation de la gestion des contacts
-router(new Contacts({readMode: 'original'}));
